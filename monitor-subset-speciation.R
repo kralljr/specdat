9f0d4ba695c31344b279d9bd2ea7e4314bda6118
Jenna Krall <jenna@Jennas-MacBook-Pro.local>
2018-12-12 15:53:54 -0500

    Deal with missing, MDL, exclusion (in the works)

--- monitor-subset-speciation.R
+++ monitor-subset-speciation.R
@@ -34,24 +34,34 @@ save(datall, file = "data/datall-monsubset.RData")
 
 
 # separate into list by monitor 
-datall1 <- select(datall, id, Date.Local, Parameter.Name, Sample.Measurement, Uncertainty)
+datall1 <- select(datall, id, Date.Local, Parameter.Name, 
+                  Sample.Measurement, Uncertainty, Detection.Limit)
 # Keep only desired constituents
 params <- unique(datall1$Parameter.Name)
 drop <- params[c(grep("OC", params), grep("EC", params), grep("OP", params))]
 drop <- drop[-grep("-adj", drop)]
 datall1 <- filter(datall1, !(Parameter.Name %in% drop))
 
+# Fix numeric detection limit
+datall1 <- mutate(datall1, Detection.Limit = as.numeric(Detection.Limit))
+
 # measurement/uncertainty
-meas <- select(datall1, -Uncertainty) %>%
+meas <- select(datall1, -Uncertainty, -Detection.Limit) %>%
   group_by(., id, Date.Local, Parameter.Name) %>%
   # Need to average by monitor?  Some EC with two measurements for same day?
-  summarize(., meas = mean(Sample.Measurement)) %>%
+  summarize(., meas = mean(Sample.Measurement, na.rm = T)) %>%
   spread(., Parameter.Name, meas) %>%
   ungroup()
-unc <- select(datall1, -Sample.Measurement) %>%
+unc <- select(datall1, -Sample.Measurement, -Detection.Limit) %>%
   group_by(., id, Date.Local, Parameter.Name) %>%
   # Need to average by monitor?  Some EC with two measurements for same day?
-  summarize(., meas = mean(Uncertainty)) %>%
+  summarize(., meas = mean(Uncertainty, na.rm = T)) %>%
+  spread(., Parameter.Name, meas) %>%
+  ungroup()
+dl <- select(datall1, -Sample.Measurement, -Uncertainty) %>%
+  group_by(., id, Date.Local, Parameter.Name) %>%
+  # Need to average by monitor?  Some EC with two measurements for same day?
+  summarize(., meas = mean(Detection.Limit, na.rm = T)) %>%
   spread(., Parameter.Name, meas) %>%
   ungroup()
 
@@ -62,10 +72,36 @@ for(i in 1 : length(unid)) {
   # create separate measurement, uncertainty files
   meas1 <- filter(meas, id == unid[i]) %>% select(., -id)
   unc1 <- filter(unc, id == unid[i]) %>% select(., -id)
+  dl1 <- filter(dl, id == unid[i]) %>% select(., -id)
   
-
-  meas1 <- meas1[complete.cases(meas1), ]
-  unc1 <- unc1[complete.cases(unc1), ]
+  # replace missing DL with median
+  wh1 <- which(colnames(dl1) == "Date.Local")
+  meds <- apply(dl1[, -wh1], 2, median, na.rm = T)
+  # ** here ... how to replace missing DL
+  
+  
+  # only if majors are missing or BDL
+  #meas1 <- meas1[complete.cases(meas1), ]
+  names1 <- c("badj-Ammonium Ion PM2.5 LC", "badj-Total Nitrate PM2.5 LC",
+              "badj-Sulfate PM2.5 LC", "badj-adjEC", "badj-adjOC")
+  cs <- colSums(meas1[, names1])
+  # Save non-missing
+  wh1 <- which(!is.na(cs))
+  meas1 <- meas1[wh1, ]
+  
+  # ** here: exclude BDL on 5
+  
+  # ** here: replace BDL metals with 1/2 MDL
+  
+  # ** here: replace missing metals with geomean
+  
+  # ** here: replace negative/0 with 1/2 MDL
+  
+  # ** Missing uncertainty
+  
+  # restrict uncertainties to same rows
+  unc1 <- unc1[wh1, ]
+  
   print(c(unid[i], nrow(meas1), nrow(unc1)))
   # for those with mroe than 50 complete observations, save data
   if(nrow(meas1) > 50) {
